5f549b726c018038a65bf6f2692a4439a8b6fb60
Marco Colia <info@justoverclock.it>
2021-11-10 11:27:18 +0100

    refactoring code avoid innerhtml

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -1,2 +1,240 @@
-module.exports=function(t){var e={};function r(n){if(e[n])return e[n].exports;var o=e[n]={i:n,l:!1,exports:{}};return t[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=t,r.c=e,r.d=function(t,e,n){r.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:n})},r.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)r.d(n,o,function(e){return t[e]}.bind(null,o));return n},r.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return r.d(e,"a",e),e},r.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},r.p="",r(r.s=4)}([function(t,e){t.exports=flarum.core.compat["forum/app"]},,function(t,e){t.exports=flarum.core.compat["common/extend"]},function(t,e){t.exports=flarum.core.compat["forum/components/DiscussionHero"]},function(t,e,r){"use strict";r.r(e);var n=r(0),o=r.n(n),i=r(2),s=r(3),a=r.n(s);o.a.initializers.add("justoverclock/theaudiodb-api",(function(){Object(i.extend)(a.a.prototype,["oncreate"],(function(){var t=o.a.session.user,e=this.attrs.discussion.title().split(/\s+/).join("%20"),r=o.a.forum.attribute("justoverclock-theaudiodb-api.langCode")||"EN";if(t)fetch("https://theaudiodb.com/api/v1/json/1/search.php?s="+e).then((function(t){if(!t.ok)throw Error(t.statusText);return t})).then((function(t){return t.json()})).then((function(t){m.redraw();var e="strBiography"+r;null===t.artists[0][e]&&(t.artists[0][e]="Oops! Description is not available in your language unfortunately...You can contribute to TheAudioDB.com by adding information about this artists in your language.");document.getElementById("descArtist").innerText=t.artists[0][e].substring(0,400)+"...",document.getElementById("imgArtists").src=t.artists[0].strArtistThumb,document.getElementById("genreArtist").innerText=o.a.translator.trans("justoverclock-theaudiodb-api.forum.genres")+": "+t.artists[0].strStyle,document.getElementById("yearBorn").innerText=o.a.translator.trans("justoverclock-theaudiodb-api.forum.intFormedYear")+": "+t.artists[0].intFormedYear,document.getElementById("strcountry").innerText=o.a.translator.trans("justoverclock-theaudiodb-api.forum.countryart")+": "+t.artists[0].strCountry})).catch((function(t){return console.log("This Artist does not exist ;) =>",e)}));Object(i.extend)(a.a.prototype,"items",(function(t){o.a.session.user&&t.add("artistDetailMusic",m("div",{class:"artistWrapper"},m("div",{id:"containerArtist"},m("div",{id:"contentArtist"},m("p",{class:"artistDesc",id:"descArtist"}),m("div",{class:"itemdescrip"},m("div",{class:"genreArtist",id:"genreArtist"}),m("div",{class:"yearBorn",id:"yearBorn"}),m("div",{class:"strcountry",id:"strcountry"})))),m("div",{id:"sidebarImgArtist"},m("img",{className:"imgArtist",id:"imgArtists",src:""}))))}))}))}))}]);
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./forum.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./forum.js":
+/*!******************!*\
+  !*** ./forum.js ***!
+  \******************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _src_forum__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./src/forum */ "./src/forum/index.js");
+/* empty/unused harmony star reexport */
+
+/***/ }),
+
+/***/ "./src/forum/index.js":
+/*!****************************!*\
+  !*** ./src/forum/index.js ***!
+  \****************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var flarum_forum_app__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! flarum/forum/app */ "flarum/forum/app");
+/* harmony import */ var flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(flarum_forum_app__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flarum/common/extend */ "flarum/common/extend");
+/* harmony import */ var flarum_common_extend__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var flarum_forum_components_DiscussionHero__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! flarum/forum/components/DiscussionHero */ "flarum/forum/components/DiscussionHero");
+/* harmony import */ var flarum_forum_components_DiscussionHero__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(flarum_forum_components_DiscussionHero__WEBPACK_IMPORTED_MODULE_2__);
+/*
+  __ _                              _ _
+ / _| |                            (_) |
+| |_| | __ _ _ __ _   _ _ __ ___    _| |_
+|  _| |/ _` | '__| | | | '_ ` _ \  | | __|
+| | | | (_| | |  | |_| | | | | | |_| | |_
+|_| |_|\__,_|_|   \__,_|_| |_| |_(_)_|\__|
+     https://flarum.it - Marco Colia
+ */
+
+
+
+flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.initializers.add('justoverclock/theaudiodb-api', function () {
+  Object(flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__["extend"])(flarum_forum_components_DiscussionHero__WEBPACK_IMPORTED_MODULE_2___default.a.prototype, 'oncreate', function () {
+    var _this = this;
+
+    var artistTitle = this.attrs.discussion.title().split(/\s+/).join('%20');
+    var languageCode = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
+    var isLoggedIn = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.session.user; // gestiamo gli errori nella risposta
+
+    function handleErrors(response) {
+      if (!response.ok) {
+        console.clear();
+        throw Error(response.statusText);
+      }
+
+      return response;
+    } // per evitare troppe richieste all'api, effettuiamo fetch solo per chi è registrato
+
+
+    if (isLoggedIn) {
+      var ArtistApi = fetch('https://theaudiodb.com/api/v1/json/1/search.php?s=' + artistTitle).then(handleErrors).then(function (response) {
+        return response.json();
+      }).then(function (data) {
+        _this.audiodb = data;
+        m.redraw();
+        var arrayElem = 'strBiography' + languageCode;
+
+        if (data.artists[0][arrayElem] === null) {
+          data.artists[0][arrayElem] = 'Oops! Description is not available in your language unfortunately...You can contribute to TheAudioDB.com by adding information about this artists in your language.';
+        }
+      })["catch"](function (error) {
+        return console.log('This Game title does not exist ;) =>', artistTitle);
+      });
+    }
+  });
+  Object(flarum_common_extend__WEBPACK_IMPORTED_MODULE_1__["extend"])(flarum_forum_components_DiscussionHero__WEBPACK_IMPORTED_MODULE_2___default.a.prototype, 'items', function (items) {
+    var languageCode = flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
+    var arrayElem = 'strBiography' + languageCode;
+    console.log(this.audiodb); // non mostriamo l'html se non c'è nulla da mostrare
+
+    if (this.audiodb === undefined) {
+      return;
+    } else {
+      items.add('artistDetailMusic', m("div", {
+        "class": "artistWrapper"
+      }, m("div", {
+        id: "containerArtist"
+      }, m("div", {
+        id: "contentArtist"
+      }, m("p", {
+        "class": "artistDesc",
+        id: "descArtist"
+      }, this.audiodb.artists[0][arrayElem].substring(0, 400) + '...'), m("div", {
+        "class": "itemdescrip"
+      }, m("div", {
+        "class": "genreArtist",
+        id: "genreArtist"
+      }, flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.translator.trans('justoverclock-theaudiodb-api.forum.genres'), ": ", this.audiodb.artists[0].strGenre), m("div", {
+        "class": "yearBorn",
+        id: "yearBorn"
+      }, flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.translator.trans('justoverclock-theaudiodb-api.forum.intFormedYear'), ": ", this.audiodb.artists[0].intFormedYear), m("div", {
+        "class": "strcountry",
+        id: "strcountry"
+      }, flarum_forum_app__WEBPACK_IMPORTED_MODULE_0___default.a.translator.trans('justoverclock-theaudiodb-api.forum.countryart'), ": ", this.audiodb.artists[0].strCountry)))), m("div", {
+        id: "sidebarImgArtist"
+      }, m("img", {
+        className: "imgArtist",
+        id: "imgArtists",
+        src: this.audiodb.artists[0].strArtistThumb
+      }))));
+    }
+  });
+});
+
+/***/ }),
+
+/***/ "flarum/common/extend":
+/*!******************************************************!*\
+  !*** external "flarum.core.compat['common/extend']" ***!
+  \******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['common/extend'];
+
+/***/ }),
+
+/***/ "flarum/forum/app":
+/*!**************************************************!*\
+  !*** external "flarum.core.compat['forum/app']" ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['forum/app'];
+
+/***/ }),
+
+/***/ "flarum/forum/components/DiscussionHero":
+/*!************************************************************************!*\
+  !*** external "flarum.core.compat['forum/components/DiscussionHero']" ***!
+  \************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = flarum.core.compat['forum/components/DiscussionHero'];
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=forum.js.map
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,6 +5,7 @@
 |  _| |/ _` | '__| | | | '_ ` _ \  | | __|
 | | | | (_| | |  | |_| | | | | | |_| | |_
 |_| |_|\__,_|_|   \__,_|_| |_| |_(_)_|\__|
+     https://flarum.it - Marco Colia
  */
 
 import app from 'flarum/forum/app';
@@ -12,64 +13,63 @@ import { extend } from 'flarum/common/extend';
 import DiscussionHero from 'flarum/forum/components/DiscussionHero';
 
 app.initializers.add('justoverclock/theaudiodb-api', () => {
-  extend(DiscussionHero.prototype, ['oncreate'], function () {
-    const isLoggedIn = app.session.user;
+  extend(DiscussionHero.prototype, 'oncreate', function () {
     const artistTitle = this.attrs.discussion.title().split(/\s+/).join('%20');
     const languageCode = app.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
+    const isLoggedIn = app.session.user;
 
     // gestiamo gli errori nella risposta
     function handleErrors(response) {
       if (!response.ok) {
+        console.clear()
         throw Error(response.statusText);
       }
       return response;
     }
 
+    // per evitare troppe richieste all'api, effettuiamo fetch solo per chi è registrato
     if (isLoggedIn) {
-      const artistApi = fetch('https://theaudiodb.com/api/v1/json/1/search.php?s=' + artistTitle)
+      const ArtistApi = fetch('https://theaudiodb.com/api/v1/json/1/search.php?s=' + artistTitle)
         .then(handleErrors)
         .then((response) => response.json())
         .then((data) => {
+          this.audiodb = data;
           m.redraw();
           const arrayElem = 'strBiography' + languageCode;
           if (data.artists[0][arrayElem] === null) {
             data.artists[0][arrayElem] =
               'Oops! Description is not available in your language unfortunately...You can contribute to TheAudioDB.com by adding information about this artists in your language.';
           }
-          const descArt = (document.getElementById('descArtist').innerText = data.artists[0][arrayElem].substring(0, 400) + '...');
-          const thumbArtists = (document.getElementById('imgArtists').src = data.artists[0].strArtistThumb);
-          const genreArtist = (document.getElementById('genreArtist').innerText =
-            app.translator.trans('justoverclock-theaudiodb-api.forum.genres') + ': ' + data.artists[0].strStyle);
-          const yearBorn = (document.getElementById('yearBorn').innerText =
-            app.translator.trans('justoverclock-theaudiodb-api.forum.intFormedYear') + ': ' + data.artists[0].intFormedYear);
-          const countryArtist = (document.getElementById('strcountry').innerText =
-            app.translator.trans('justoverclock-theaudiodb-api.forum.countryart') + ': ' + data.artists[0].strCountry);
         })
-        .catch((error) => console.log('This Artist does not exist ;) =>', artistTitle));
+        .catch((error) => console.log('This Game title does not exist ;) =>', artistTitle));
     }
-
-    extend(DiscussionHero.prototype, 'items', function (items) {
-      const isLoggedIn = app.session.user;
-      if (isLoggedIn) {
-        items.add(
-          'artistDetailMusic',
-          <div class="artistWrapper">
-            <div id="containerArtist">
-              <div id="contentArtist">
-                <p class="artistDesc" id="descArtist"/>
-                <div class="itemdescrip">
-                  <div class="genreArtist" id="genreArtist"/>
-                  <div class="yearBorn" id="yearBorn"/>
-                  <div class="strcountry" id="strcountry"/>
-                </div>
+  });
+  extend(DiscussionHero.prototype, 'items', function (items) {
+    const languageCode = app.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
+    const arrayElem = 'strBiography' + languageCode;
+    console.log(this.audiodb)
+    // non mostriamo l'html se non c'è nulla da mostrare
+    if (this.audiodb === undefined) {
+      return;
+    } else {
+      items.add(
+        'artistDetailMusic',
+        <div class="artistWrapper">
+          <div id="containerArtist">
+            <div id="contentArtist">
+              <p class="artistDesc" id="descArtist">{this.audiodb.artists[0][arrayElem].substring(0,400) + '...'}</p>
+              <div class="itemdescrip">
+                <div class="genreArtist" id="genreArtist">{app.translator.trans('justoverclock-theaudiodb-api.forum.genres')}: {this.audiodb.artists[0].strGenre}</div>
+                <div class="yearBorn" id="yearBorn">{app.translator.trans('justoverclock-theaudiodb-api.forum.intFormedYear')}: {this.audiodb.artists[0].intFormedYear}</div>
+                <div class="strcountry" id="strcountry">{app.translator.trans('justoverclock-theaudiodb-api.forum.countryart')}: {this.audiodb.artists[0].strCountry}</div>
               </div>
             </div>
-            <div id="sidebarImgArtist">
-              <img className="imgArtist" id="imgArtists" src=""/>
-            </div>
           </div>
-        );
-      }
-    });
+          <div id="sidebarImgArtist">
+            <img className="imgArtist" id="imgArtists" src={this.audiodb.artists[0].strArtistThumb}/>
+          </div>
+        </div>
+      );
+    }
   });
 });
