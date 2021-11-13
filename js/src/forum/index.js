a62d3b8f18de77ec1e04d6a49747253301916c0a
Marco Colia <info@justoverclock.it>
2021-11-13 12:20:49 +0100

    fixed api errors

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -13,63 +13,62 @@ import { extend } from 'flarum/common/extend';
 import DiscussionHero from 'flarum/forum/components/DiscussionHero';
 
 app.initializers.add('justoverclock/theaudiodb-api', () => {
-  extend(DiscussionHero.prototype, 'oncreate', function () {
+  extend(DiscussionHero.prototype, 'oncreate', async function () {
     const artistTitle = this.attrs.discussion.title().split(/\s+/).join('%20');
+
     const languageCode = app.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
     const isLoggedIn = app.session.user;
 
-    // gestiamo gli errori nella risposta
-    function handleErrors(response) {
-      if (!response.ok) {
-        console.clear()
-        throw Error(response.statusText);
-      }
-      return response;
-    }
-
     // per evitare troppe richieste all'api, effettuiamo fetch solo per chi è registrato
     if (isLoggedIn) {
-      const ArtistApi = fetch('https://theaudiodb.com/api/v1/json/1/search.php?s=' + artistTitle)
-        .then(handleErrors)
-        .then((response) => response.json())
+      const ArtistApi = await fetch('https://theaudiodb.com/api/v1/json/1/search.php?s=' + artistTitle)
+        .then(async (response) => await response.json())
         .then((data) => {
           this.audiodb = data;
           m.redraw();
           const arrayElem = 'strBiography' + languageCode;
           if (data.artists[0][arrayElem] === null) {
             data.artists[0][arrayElem] =
-              'Oops! Description is not available in your language unfortunately...You can contribute to TheAudioDB.com by adding information about this artists in your language.';
+              'Oops! Description is not available in your language unfortunately. You can contribute to TheAudioDB.com by adding information about this artists in your language.';
           }
         })
-        .catch((error) => console.log('This Game title does not exist ;) =>', artistTitle));
+        .catch((error) => console.log('This Artist/band does not exist ;) =>', artistTitle));
+    }
+
+    if (this.audiodb.artists === null) {
+      const el = document.querySelector('li.item-artistDetailMusic');
+      el.style.display = 'none';
+    } else {
+      const arrayElem = 'strBiography' + languageCode;
+      const Genre = (document.getElementById('genreArtist').innerText =
+        app.translator.trans('justoverclock-theaudiodb-api.forum.genres') + ': ' + this.audiodb.artists[0].strGenre);
+      const DescArt = (document.getElementById('descArtist').innerText = this.audiodb.artists[0][arrayElem].substring(0, 400) + '...');
+      const yearB = (document.getElementById('yearBorn').innerText =
+        app.translator.trans('justoverclock-theaudiodb-api.forum.intFormedYear') + ': ' + this.audiodb.artists[0].intFormedYear);
+      const Country = (document.getElementById('strcountry').innerText =
+        app.translator.trans('justoverclock-theaudiodb-api.forum.countryart') + ': ' + this.audiodb.artists[0].strCountry);
+      const urlImg = this.audiodb.artists[0].strArtistThumb;
+      $('#imgArtists').attr('src', urlImg);
     }
   });
   extend(DiscussionHero.prototype, 'items', function (items) {
-    const languageCode = app.forum.attribute('justoverclock-theaudiodb-api.langCode') || 'EN';
-    const arrayElem = 'strBiography' + languageCode;
-    console.log(this.audiodb)
-    // non mostriamo l'html se non c'è nulla da mostrare
-    if (this.audiodb === undefined) {
-      return;
-    } else {
-      items.add(
-        'artistDetailMusic',
-        <div class="artistWrapper">
-          <div id="containerArtist">
-            <div id="contentArtist">
-              <p class="artistDesc" id="descArtist">{this.audiodb.artists[0][arrayElem].substring(0,400) + '...'}</p>
-              <div class="itemdescrip">
-                <div class="genreArtist" id="genreArtist">{app.translator.trans('justoverclock-theaudiodb-api.forum.genres')}: {this.audiodb.artists[0].strGenre}</div>
-                <div class="yearBorn" id="yearBorn">{app.translator.trans('justoverclock-theaudiodb-api.forum.intFormedYear')}: {this.audiodb.artists[0].intFormedYear}</div>
-                <div class="strcountry" id="strcountry">{app.translator.trans('justoverclock-theaudiodb-api.forum.countryart')}: {this.audiodb.artists[0].strCountry}</div>
-              </div>
+    items.add(
+      'artistDetailMusic',
+      <div class="artistWrapper">
+        <div id="containerArtist">
+          <div id="contentArtist">
+            <p class="artistDesc" id="descArtist" />
+            <div class="itemdescrip">
+              <div class="genreArtist" id="genreArtist" />
+              <div class="yearBorn" id="yearBorn" />
+              <div class="strcountry" id="strcountry" />
             </div>
           </div>
-          <div id="sidebarImgArtist">
-            <img className="imgArtist" id="imgArtists" src={this.audiodb.artists[0].strArtistThumb}/>
-          </div>
         </div>
-      );
-    }
+        <div id="sidebarImgArtist">
+          <img className="imgArtist" id="imgArtists" src="" />
+        </div>
+      </div>
+    );
   });
 });
